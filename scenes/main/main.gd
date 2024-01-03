4c2d8d6879354d16fe395b394f1c2cdbf857b158
jan <admin@nanologic.dev>
2024-01-03 15:57:50 +0100

    Change level to world when joining

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -13,6 +13,7 @@ func _on_host_pressed():
 	add_player()
 	cam.enabled = false
 	canvasLayer.visible = false
+	change_level.call_deferred(load("res://scenes/world/world.tscn"))
 
 
 func _on_join_pressed():
@@ -20,7 +21,7 @@ func _on_join_pressed():
 	multiplayer.multiplayer_peer = peer
 	cam.enabled = false
 	canvasLayer.visible = false
-
+	change_level.call_deferred(load("res://scenes/world/world.tscn"))
 
 func add_player(id = 1):
 	var player = player_scene.instantiate()
@@ -39,3 +40,14 @@ func del_player(id):
 
 @rpc("any_peer", "call_local") func _del_player(id):
 	get_node(str(id)).queue_free()
+
+
+# Call this function deferred and only on the main authority (server).
+func change_level(scene: PackedScene):
+	# Remove old level if any.
+	var level = $Level
+	for c in level.get_children():
+		level.remove_child(c)
+		c.queue_free()
+	# Add new level.
+	level.add_child(scene.instantiate())
