625c7982c25ec9b8f3df2a31793b854cbd7501a6
jan <admin@nanologic.dev>
2024-01-03 18:25:15 +0100

    better player character (animations)

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,16 +1,44 @@
 extends CharacterBody2D
 
 @onready var cam = $Camera2D
+@onready var animatedSprite = $AnimatedSprite2D
 
-const SPEED = 300.0
+var action = "idle"
+var looking = "front"
+var direction = "left"
+
+const SPEED = 50.0
 
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 
+
 func _ready():
 	cam.enabled = is_multiplayer_authority()
+	animatedSprite.play("idle_front_left")
 
-func _physics_process(delta):
+
+func _physics_process(_delta):
 	if is_multiplayer_authority():
 		velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * SPEED
-	move_and_slide()
+
+
+func _process(_delta):
+	if is_multiplayer_authority():
+		move_and_slide()
+		
+		if velocity == Vector2(0, 0):
+			action = "idle"
+		else:
+			action = "walk"
+			if velocity.x > 0:
+				direction = "right"
+			elif velocity.x < 0:
+				direction = "left"
+			
+			if velocity.y >= 0:
+				looking = "front"
+			elif velocity.y < 0:
+				looking = "back"
+		
+		animatedSprite.play(action + "_" + looking + "_" + direction)
