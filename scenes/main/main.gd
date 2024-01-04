dc0640d89e7715e0c0a229be75c0a5c3c017501c
jan <admin@nanologic.dev>
2024-01-04 22:17:12 +0100

    Implement basic dedicated_server

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -4,25 +4,35 @@ var peer = ENetMultiplayerPeer.new()
 var scene = preload("res://scenes/world/world.tscn").instantiate()
 @onready var cam = $Camera2D
 @onready var canvasLayer = $Camera2D/CanvasLayer
+@onready var ip = $Camera2D/CanvasLayer/CenterContainer/VBoxContainer/IP
+
+func _ready():
+	if DisplayServer.get_name() == "headless":
+		print("Started in headless mode.")
+		create_server()
 
 func _on_host_pressed():
-	get_tree().root.add_child(scene)
-	peer.create_server(123)
-	multiplayer.multiplayer_peer = peer
-	multiplayer.peer_connected.connect(add_player)
+	create_server()
 	add_player()
 	hideUi()
 
 
 func _on_join_pressed():
-	get_tree().root.add_child(scene)
-	peer.create_client("127.0.0.1", 123)
+	get_tree().get_root().call_deferred("add_child", scene)
+	peer.create_client(ip.text, 123)
 	multiplayer.multiplayer_peer = peer
 	hideUi()
 
+func create_server():
+	get_tree().get_root().call_deferred("add_child", scene)
+	peer.create_server(123)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(add_player)
+	print("Server created")
 
 func add_player(id = 1):
-		scene.spawn_player(id)
+	scene.spawn_player(id)
+	print("Added player: " + str(id))
 
 
 func exit_game(id):
