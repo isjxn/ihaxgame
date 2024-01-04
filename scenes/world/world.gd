925fe33db5257752c66a35c8c8f8b0d943d91f0a
jan <admin@nanologic.dev>
2024-01-04 21:17:22 +0100

    Fix player spawn point

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var player_scene : PackedScene
 
+@onready var spawnPoint = $SpawnPoint
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +12,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_child_entered_tree(node):
+	if node.is_in_group("players"):
+		node.position = spawnPoint.position
 
 
 func spawn_player(id):
